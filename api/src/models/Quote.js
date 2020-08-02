de157cda54bd7f69d3351854df8a7364106e2d03
msieurmoustache <fbrobillard@gmail.com>
2020-08-02 13:39:28 -0400

    change quotes

--- api/src/models/Quote.js
+++ api/src/models/Quote.js
@@ -2,6 +2,9 @@
 const mongoose = require('mongoose')
 
 const quoteSchema = new mongoose.Schema({
+  number: Number,
+  date: Date,
+  text: String
 })
 
 module.exports = mongoose.model('Quote', quoteSchema)
--- app/src/pages/addQuote.js
+++ app/src/pages/addQuote.js
@@ -1,13 +1,24 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "../components/layout"
 import { navigate } from "gatsby"
 import QuotesClient from "../utils/quotesClient";
 
 const AddQuote = () => {
     const quoteClient = new QuotesClient();
-    const [quote, setQuote] = useState("");
+    const [quote, setQuote] = useState({text:"", date: new Date(), number: 0});
+
+    const setNumber = async() => {
+        var nb = await quoteClient.getQuotes().length;
+        setQuote({...quote, number: nb+1});
+    }
+
+    useEffect(() => {
+        setNumber();
+    }, [])
 
     const submitQuote = async() => {
+        await setNumber();
+
         await quoteClient.submitQuote(quote);
         navigate("/");
     }
@@ -18,7 +29,7 @@ const AddQuote = () => {
             <form>
                 <h2>Ajout d'une citation:</h2>
                 <p>
-                    <textarea rows="10" cols="80" name="quote" value={quote} onChange={(e) => setQuote(e.target.value)} /><br />
+                    <textarea rows="10" cols="80" name="quote" value={quote.text} onChange={(e) => setQuote({...quote, text: e.target.value})} /><br />
                     <input type="button" value="Add Quote" onClick={submitQuote} />
                 </p>
             </form>
