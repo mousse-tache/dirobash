5d3d30e5c66af65c58582f4717c77e1702b8b9de
msieurmoustache <fbrobillard@gmail.com>
2024-09-02 11:15:56 -0400

    .

--- app/src/pages/IndexPage.tsx
+++ app/src/pages/IndexPage.tsx
@@ -5,34 +5,34 @@ import QuotesClient from "../utils/quotesClient"
 import { useSearchParams } from "react-router-dom";
 
 const IndexPage = () => {
-  const quotesClient = new QuotesClient();
 
     const [quotes, setQuotes] = useState([{text: "Nothing to see here", date: new Date().toLocaleDateString(), number: 0}]);
     const [count, setCount] = useState(1);
     const [searchParams, setSearchParams] = useSearchParams();
-
-    async function FetchQuotes() {
-        try {
-            let { data } = await quotesClient.getQuotes(searchParams.get("page")?? 1);
-
-            if (data.data !== null && data.data.length > 0) {
-                setQuotes(data.data);
-            }
-
-            if (data.count !== null && data.count > 1) {
-              setCount(data.count);
-          }
-        } catch (error) {
-            console.log("Oh well, we didn't even need those quotes anyways")
-        }
-    }
 
     async function setPage(pageNumber: string) {
       setSearchParams({"page":pageNumber})
     }
 
     useEffect(() => {
-        FetchQuotes();
+        async function FetchQuotes() {
+          try {
+              const quotesClient = new QuotesClient();
+              let { data } = await quotesClient.getQuotes(searchParams.get("page")?? 1);
+
+              if (data.data !== null && data.data.length > 0) {
+                  setQuotes(data.data);
+              }
+
+              if (data.count !== null && data.count > 1) {
+                setCount(data.count);
+            }
+          } catch (error) {
+              console.log("Oh well, we didn't even need those quotes anyways")
+          }
+      }
+
+      FetchQuotes();
     }, [searchParams])
 
   return (
